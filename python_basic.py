9fe54ba7cc72bb11d04d50a779c2194539030824
Daeheon <shew1903@gmail.com>
2022-05-19 23:00:13 +0900

    usefull Functions

--- python_basic.py
+++ python_basic.py
@@ -220,31 +220,122 @@ def my_key(x) :
 print(sorted(array, key=my_key))            #sorted(리스트, key속성(정렬기준을 명시 할 수 있음))
 print(sorted(array, key=lambda x:x[1]))
 
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,10]
 
+result = map(lambda a,b : a+b, list1, list2)        #map = 각각의 원소에 어떤 함수를 적용하려고 할때
+print(list(result))             
 
+'''
+****************************************************************************
+실전에서 유용한 표준 라이브러리
 
+내장함수 : 기본 입출력 함수부터 정렬함수까지 기본적인 함수 제공
+- 필수 기능 포함되어있음
+itertools : 파이썬에서 반복되는 형태의 데이터를 처리하기 위한 유용한 기능 제공
+- 특히 순열과 조합 라이브러리는 코딩테스트에서 자주 사용됨                  //모든 경우의수 or 완전탐색에서 소스코드를 간결하게 작성하는데 도움됨
+heapq : heap 자료구조 제공
+- 일반적으로 우선순위 큐 기능을 구현하기 위해 사용                          //최단경로 알고리즘에서 사용됨
+bisect : 이진탐색(binary search)기능 제공                               // 기본적인 이진탐색 사용됨
+collections : 덱(deque), 카운터(counter) 등의 유용한 자료구조를 포함    
+math : 필수적인 수학적 기능 제공
+- 팩토리얼, 제곱근, 최대공약수(GCD), 삼각함수 관련 함수부터 파이(pi)와 같은 상수를 포함
+****************************************************************************
+'''
 
+#자주 사용되는 내장 함수
+#sum()                      #//원소들의 합 반환
+result = sum([1,2,3,4,5])
+print(result)
 
+#min(), max()
+min_result = min(7,3,5,2)
+max_result = max(7,3,5,2)
+print(min_result,max_result)
 
+#eval()                     #//사람의 입장에서 하나의 식이 있을경우 식을 계산하여 반환함
+result = eval("(3+5)*7")
+print(result)
+print(type(result))
 
+#sorted()
+result = sorted([9,1,8,1,5,3,4])    #올림차순
+reverse_result = sorted([9,1,8,1,5,3,4], reverse = True)    #내림차순
+print(result)
+print(reverse_result)
 
+#sorted() with key
+array = [('홍길동',50), ('이순신',32), ('아무개',74)]
+result = sorted(array, key=lambda x:x[1], reverse = True)   #x[1]가 key값(정렬기준), 내림차순
+print(result)
 
+'''
+모든 경우의 수를 고려해야 할때 어떤 라이브러리를 효과적으로 사용할 수 있을까요?
+순열 : 서로 다른 n개에서 서로 다른 r개를 선택하여 일렬로 나열하는 것
+- {'A','B','C'}에서 세 개를 선택하여 나열하는 경우: 'ABC','ACB','BAC','CAB','CBA'
+조합 : 서로 다른 n개에서 순서에 상관 없이 서로 다른 r개를 선택하는 것
+- {'A','B','C'}에서 순서를 고려하지 않고 두개를 뽑는 경우 : 'AB','AC','BC'
 
+순열의 수 : nPr = n * (n-1) * (n-2) *...* (n-r+1)
+조합의 수 : nCr = { n * (n-1) * (n-2) *...* (n-r+1) } / r!
+'''
+#순열
+# - {'A','B','C'}에서 세 개를 선택하여 나열하는 경우: 'ABC','ACB','BAC','CAB','CBA'
+from itertools import permutations
+data = ['A','B','C']
+result = list(permutations(data,3))     #튜플로 저장됨
+print(result)
 
+###########################################################################
+#조합
+# - {'A','B','C'}에서 순서를 고려하지 않고 두개를 뽑는 경우 : 'AB','AC','BC'
+from itertools import combinations
+data = ['A','B','C']
+result = list(combinations(data,2))     #튜블로 저장됨
+print(result)
 
+#중복 순열과 중복 조합
+from itertools import product
+data = ['A','B','C']
+result = list(product(data, repeat = 2))        #2개를 뽑는 모든 순열 구하기(중복 허용)
+print(result)
 
+from itertools import combinations_with_replacement
+data = ['A','B','C']
+result = list(combinations_with_replacement(data,2)) #2개를 뽑는 모든 조합 구하기 (중복 허용)
+print(result)
+###########################################################################
 
+'''
+Counter
+파이썬 collections 라이브러리의 Counter는 등장횟수를 세는 기능을 제공합니다.
+리스트와 같은 반복 가능한(iterable)객체가 주어졌을 때 내부의 원소가 몇 번씩 등장했는지 알려줍니다.
+워드 클라우드와 같은 걸 만들때 매우 유용함
+'''
+from collections import Counter
+counter = Counter(['red','blue','red','green','blue','blue'])
+print(counter['blue'])  #blue의 등장 횟수 출력
+print(counter['green']) #green의 등장 횟수 출력
+print(dict(counter))    #사전 자료형으로 반환
 
+###########################################################################
+'''
+math 라이브러리
+원주율과 같은 함수도 포함하고 있음
+최대공약수를 구할때는 gcd() 함수
+'''
+#최소공배를 구하는 함수
+import math
+def lcm(a,b):
+    return a*b // math.gcd(a,b)
 
+a= 21
+b= 14
 
+print(math.gcd(21,14))
+print(lcm(21,14))
 
-
-
-
-
-
-
-
+###########################################################################
 
 
 
